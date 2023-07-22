a1419d19deaf53d5f01f5755c0d393b15bdf74bb
zana-abdollahpour <zana.ap4@gmail.com>
2023-07-22 23:35:43 +0330

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from "./Header";
+import Main from "./Main";
+
+export default function App() {
+  return (
+    <div className="app">
+      <Header />
+
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
+    </div>
+  );
+}
